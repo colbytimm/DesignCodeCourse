31a7859635d5a1bbdeb24997074b1bde8eaaa82c
colbytimm <timmcolby@gmail.com>
2020-12-06 11:29:37 -0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Colby Timm on 2020-12-06.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
